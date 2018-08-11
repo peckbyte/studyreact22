8e28de9ad03eba3b83d32d2eaaabce76e737bc14
MacBook Air <macbookair@MacBookdeMacBook-Air.local>
2018-08-11 17:02:58 +0800

    2.5服务端渲染，成功

---
+++ build/webpack.config.server.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    target:'node',
+    entry:{
+        app: path.join(__dirname,'../client/server-entry.js')
+    },
+    output:{
+        filename: 'server-entry.js',
+        path: path.join(__dirname,'../dist'),
+        publicPath: '',
+        libraryTarget:'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader:'babel-loader'
+            },
+            {
+                test:/.js$/,
+                loader:'babel-loader',
+                exclude:[
+                    path.join(__dirname,'../node_modules')
+                ]
+            }
+        ]
+    }
+
+}
